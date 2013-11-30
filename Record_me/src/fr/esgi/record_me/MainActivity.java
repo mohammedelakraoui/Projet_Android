cb64fd67865d14f5b83a66b76af2fc101d8e4aa5
mohammedelakraoui <mohammed.elakraoui@gmail.com>
2013-11-30 18:16:48 +0100

    mettre en place la fonction record

--- Record_me/src/fr/esgi/record_me/MainActivity.java
+++ Record_me/src/fr/esgi/record_me/MainActivity.java
@@ -1,18 +1,31 @@
 package fr.esgi.record_me;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.view.Menu;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.media.MediaRecorder;
 
 public class MainActivity extends Activity {
   Button start_record;
   Button stop_record;
   AlertDialog message;
+  private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+  private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
+  private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+  private int currentFormat = 0;
+  private MediaRecorder recorder=null;
+  private int output_formats[] = { MediaRecorder.OutputFormat.MPEG_4,
+			MediaRecorder.OutputFormat.THREE_GPP };
+  private String file_exts[] = { AUDIO_RECORDER_FILE_EXT_MP4,
+			AUDIO_RECORDER_FILE_EXT_3GP };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	
@@ -26,28 +39,72 @@ public class MainActivity extends Activity {
         
 
     }
-    
+private String getFilename() {
+		String filepath = Environment.getExternalStorageDirectory().getPath();
+		File file = new File(filepath, AUDIO_RECORDER_FOLDER);
+
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+
+		return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+	}
+private void startRecording() {
+		recorder = new MediaRecorder();
+
+		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		recorder.setOutputFormat(output_formats[currentFormat]);
+		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+		recorder.setOutputFile(getFilename());
+
+	//	recorder.setOnErrorListener(errorListener);
+	//	recorder.setOnInfoListener(infoListener);
+
+		try {
+			recorder.prepare();
+			recorder.start();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 View.OnClickListener action_start= new View.OnClickListener() {
 	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		startRecording();
+	//	message.setTitle("start");
+	//	message.setMessage("Button start record.");
+	//	message.show();
+		//recorder.setAudioEncoder(MediaRecorder.AudioSource.MIC);
 		
-		message.setTitle("start");
-		message.setMessage("Button start record.");
-		message.show();
 	}
 };
+
+private void stopRecording() {
+	if (null != recorder) {
+		recorder.stop();
+		recorder.reset();
+		recorder.release();
+
+		recorder = null;
+	}
+}
+
   View.OnClickListener action_stop=new View.OnClickListener() {
 	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 	
+		stopRecording();
 		
-		message.setTitle("stop");
-		message.setMessage("Button stop record.");
-		message.show();
+		//message.setTitle("stop");
+		//message.setMessage("Button stop record.");
+		//message.show();
 	}
 };  
 
